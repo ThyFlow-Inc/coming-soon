d191c07ba3e88f14024253fe5d15661717b28c7e
Yusadolat <yusadolat@gmail.com>
2020-12-04 20:43:34 +0100

    Deploy fix :unicorn:

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-import React from 'react';
-import { ReactComponent as IconFacebook } from './assets/icons/facebook.svg';
-import { ReactComponent as IconTwitter } from './assets/icons/twitter.svg';
-import { ReactComponent as IconGithub } from './assets/icons/github.svg';
+import React from "react";
 import "./App.css";
 import logo from './assets/big_logo.png'; // Tell webpack this JS file uses this image
 
@@ -9,21 +6,23 @@ class App extends React.Component {
   render = () => {
     return (
       <div className="card">
-   
         <div className="content">
           <div className="title-holder">
-          <div className="logo">
-          <img src={logo} alt="Logo" className="logo" />
-          </div>
+            <div className="logo">
+              <img src={logo} alt="Logo" className="logo" />
+            </div>
             <h1>Need a haircut?</h1>
-            <p>ThyFlow connects you to the best licensed barbers in your area. Download the app, pick a barber, and they'll arrive to your home in no time!</p>
+            <p>
+              ThyFlow connects you to the best licensed barbers in your area.
+              Download the app, pick a barber, and they'll arrive to your home
+              in no time!
+            </p>
           </div>
           <input className="cta1" type="text" placeholder="Notify me First" />
           <a href="mailto:support@thyflow.com">
-            <div className="cta">Send us an email</div>
+            <div className="cta">sign me up</div>
           </a>
         </div>
-        
       </div>
     );
   }
