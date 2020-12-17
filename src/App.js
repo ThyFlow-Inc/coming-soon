4ca4b2593dec7a791fd4b9c0be86e4f7ba09062d
Yusuf Adeyemo <yusadolat@gmail.com>
2020-12-17 06:04:07 +0000

    remove background :zap:

--- src/App.js
+++ src/App.js
@@ -9,25 +9,11 @@ class App extends React.Component {
   
   render = () => {
     return (
-      <div className="card">
-        <div className="content">
-          <div className="title-holder">
-            <div className="logo">
-              <img src={logo} alt="Logo" className="logo" />
-            </div>
-            <h1>Need a haircut?</h1>
-            <p>
-              ThyFlow connects you to the best licensed barbers in your area.
-              Download the app, pick a barber, and they'll arrive to your home
-              in no time!
-            </p>
-          </div>
+      <div>
+  
+         
           <ReactTypeformEmbed url="https://yusuf225078.typeform.com/to/jCmMc3A8" />
-                    {/* <input className="cta1" type="text" placeholder="Notify me First" />
-          <a href="mailto:support@thyflow.com">
-            <div className="cta">sign me up</div>
-          </a> */}
-        </div>
+                    
       </div>
     );
   }
