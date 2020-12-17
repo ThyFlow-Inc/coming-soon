e3ee6b83f490da074fc548ea4578046cc2d2452e
Yusuf Adeyemo <yusadolat@gmail.com>
2020-12-17 06:05:08 +0000

    reformat :zap:

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React from "react";
-import { ReactTypeformEmbed } from 'react-typeform-embed';
-import './App.css'
-import logo from './assets/big_logo.png'; // Tell webpack this JS file uses this image
-
-
+import { ReactTypeformEmbed } from "react-typeform-embed";
+import logo from "./assets/big_logo.png"; // Tell webpack this JS file uses this image
 
 class App extends React.Component {
-  
   render = () => {
     return (
       <div>
-  
-         
-          <ReactTypeformEmbed url="https://yusuf225078.typeform.com/to/jCmMc3A8" />
-                    
+        <ReactTypeformEmbed url="https://yusuf225078.typeform.com/to/jCmMc3A8" />
       </div>
     );
-  }
+  };
 }
 
 export default App;
